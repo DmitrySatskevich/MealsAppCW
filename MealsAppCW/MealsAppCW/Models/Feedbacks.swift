b74acc74a022a7f4bbff633d0f16966d60656b8e
RecrutSatsckevich <100848161+DmitrySatskevich@users.noreply.github.com>
2022-12-14 14:00:16 +0300

    UI part fulfilled

---
+++ MealsAppCW/MealsAppCW/Models/Feedbacks.swift
@@ -0,0 +1,16 @@
+//
+//  Feedbacks.swift
+//  MealsAppCW
+//
+//  Created by dzmitry on 14.12.22.
+//
+
+import Foundation
+
+struct Feedback {
+    
+    private let date = Date()
+    
+    let text: String
+    let mark: Double
+}
---
+++ MealsAppCW/MealsAppCW/Models/Meel.swift
@@ -0,0 +1,16 @@
+//
+//  Meel.swift
+//  MealsAppCW
+//
+//  Created by dzmitry on 14.12.22.
+//
+
+import UIKit
+
+struct Meel {
+    let name: String
+    let price: Double
+    let image: UIImage?
+    
+    var feedbacks: [Feedback] = []
+}
